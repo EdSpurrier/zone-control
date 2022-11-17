17202647460e1277f8e3648aa1430006d02cd1d6
Ed Spurrier <ed@edspurrier.com>
2022-11-17 23:29:00 +1100

    Added UDP client - only to external-os-controller [this needs to be moved into TitanCore so it can be used across everything]

--- external-os-controller/main.js
+++ external-os-controller/main.js
@@ -1,9 +1,43 @@
 const { TitanCore } = require('titan-core');
 const { GetWindow } = require('./utils/get-window');
 
+// MODULES
+const udpServer = require('./utils/udp-server')
+const udpClient = require('./utils/udp-client')
+
 var titanCore = new TitanCore();
 var console = titanCore.console;
 console.logError(titanCore.hello);
 
-//GetWindow();
-require('./utils/udp');
+
+//  UDP SERVER
+udpServer.setup(titanCore, 4004);
+
+// emits on new datagram msg
+udpServer.server.on('message',function(msg,info){
+    console.log('Data received from client : ' + msg.toString());
+    console.log('Received %d bytes from %s:%d\n',msg.length, info.address, info.port);
+    udpServer.close();
+});
+
+
+udpServer.start();
+
+//  UDP CLIENT
+udpClient.setup(titanCore);
+
+
+
+
+
+
+
+udpClient.send("Hello", 4004);
+
+
+
+setInterval(function(){
+    console.log(".");
+}, 1000);
+
+
